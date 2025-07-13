ada8c61e6592b05b2d76f7af6af0c33711cb1f3f
olayinkaajayi <work.for.funmilade@gmail.com>
2025-07-13 01:26:58 +0100

    set all variable that achieved a successful run with 74.14% accuracy

--- scripts/train_cambridge_stage2_dynamic.py
+++ scripts/train_cambridge_stage2_dynamic.py
@@ -2,28 +2,38 @@ import sys
 sys.path.append(".")
 from train_net_dynamic import *
 
-cfg=Config('volleyball')
-cfg.inference_module_name = 'dynamic_volleyball'
+cfg=Config('cambridge',use_root=True)
+cfg.inference_module_name = 'dynamic_cambridge'
 
-cfg.device_list = "0,1"
+cfg.device_list = "0,1,2"
 cfg.use_gpu = True
 cfg.use_multi_gpu = True
 cfg.training_stage = 2
-cfg.train_backbone = True
-cfg.test_before_train = False
+cfg.train_backbone = False
+cfg.test_before_train = True
 cfg.test_interval_epoch = 1
 
 # vgg16 setup
-cfg.backbone = 'vgg16'
-cfg.stage1_model_path = 'result/basemodel_VD_vgg16.pth'
-cfg.out_size = 22, 40
-cfg.emb_features = 512
+# cfg.backbone = 'vgg16'
+# cfg.stage1_model_path = 'result/basemodel_CamD_res18.pth'
+# cfg.out_size = 22, 40
+# cfg.emb_features = 512
 
 # res18 setup
-# cfg.backbone = 'res18'
-# cfg.stage1_model_path = 'result/basemodel_VD_res18.pth'
-# cfg.out_size = 23, 40
-# cfg.emb_features = 512
+cfg.backbone = 'res18'
+cfg.stage1_model_path = 'result/basemodel_CamD_res18.pth'
+cfg.out_size = 23, 40
+cfg.emb_features = 512
+
+cfg.num_actions=10  #number of action categories (we actually have no actions)
+cfg.num_activities=10  #number of activity categories
+
+# Added to make suitable for pytorch RoIAlign
+cfg.crop_size={'output_size':cfg.crop_size,
+        'spatial_scale':1.0/16, # Adjust based on your backbone stride
+        'sampling_ratio':2
+        }
+
 
 # Dynamic Inference setup
 cfg.group = 1
@@ -39,9 +49,10 @@ cfg.parallel_inference = False
 cfg.num_DIM = 1
 cfg.train_dropout_prob = 0.3
 
-cfg.batch_size = 2
-cfg.test_batch_size = 1
-cfg.num_frames = 10
+cfg.batch_size = 12
+cfg.test_batch_size = 6
+cfg.num_boxes = 6
+cfg.num_frames = cfg.max_video_len
 cfg.load_backbone_stage2 = True
 cfg.train_learning_rate = 1e-4
 # cfg.lr_plan = {11: 3e-5, 21: 1e-5}
@@ -49,7 +60,6 @@ cfg.train_learning_rate = 1e-4
 # cfg.lr_plan = {11: 3e-5, 21: 1e-5}
 cfg.lr_plan = {11: 1e-5}
 cfg.max_epoch = 30
-cfg.actions_weights = [[1., 1., 2., 3., 1., 2., 2., 0.2, 1.]]
 
-cfg.exp_note = 'Dynamic Volleyball_stage2_res18_litedim128_reproduce_1'
+cfg.exp_note = 'CamD-1-Subset_stage2_res18_reproduce_1'
 train_net(cfg)
