c9a3d493ae1591640d80f107dfd00518bef025fa
olayinkaajayi <work.for.funmilade@gmail.com>
2025-08-31 21:28:34 +0100

    DPI accepts just one output now

--- infer_model.py
+++ infer_model.py
@@ -211,8 +211,7 @@ class Dynamic_volleyball(nn.Module):
         # Dynamic graph inference
         # graph_boxes_features = self.DPI(boxes_features)
 
-#################### Make changes here, as only one output is returned ############ 
-        graph_boxes_features, ft_infer_MAD = self.DPI(boxes_features)
+        graph_boxes_features = self.DPI(boxes_features)
         torch.cuda.empty_cache()
 
 
